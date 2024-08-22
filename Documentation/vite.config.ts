a2c9af415970e7baaa5ba01745c7f73933a250ea
Soofa <emr3.cbc@gmail.com>
2024-08-22 22:11:46 +0300

    Small fixes

--- Documentation/vite.config.ts
+++ Documentation/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Abilities/',  // Replace 'repository-name' with your actual repository name
+  base: '/Abilities/docs',
 });
